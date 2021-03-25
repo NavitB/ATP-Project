ecbc6ecefde27656d7b8d9f52697a6d878a5dce8
navitbranin <navitbranin123@gmail.com>
2021-03-25 17:48:47 +0200

    emptyMaze

--- src/algorithms/mazeGenerators/EmptyMazeGenerator.java
+++ src/algorithms/mazeGenerators/EmptyMazeGenerator.java
@@ -4,6 +4,17 @@ public class EmptyMazeGenerator extends AMazeGenerator {
 
     @Override
     public Maze generate(int rows, int columns) {
-        return null;
+
+        int[][] m = new int[rows][columns];
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < columns; j++)
+            {
+                m[i][j] = 0;
+            }
+        }
+        Maze newMaze = new Maze(rows,columns);
+        newMaze.setMaze(m);
+        return newMaze;
     }
 }
--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -2,8 +2,23 @@ package algorithms.mazeGenerators;
 
 public class Maze {
 
+    private int numOfRows;
+    private int numOfColumns;
+    private int[][] maze;
     private Position startPosition;
     private Position goalPosition;
+
+
+    public Maze(int numOfRows, int numOfColumns) {
+
+        this.numOfRows = numOfRows;
+        this.numOfColumns = numOfColumns;
+    }
+
+    public void setMaze(int[][] maze) {
+        this.maze = maze;
+    }
+
 
     public Position getStartPosition() {
         return startPosition;
