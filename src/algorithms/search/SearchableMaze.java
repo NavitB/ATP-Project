d574b4d03d09f3e09cca1013571690d5d8174582
danahoh <danahoh3@gmail.com>
2021-03-25 17:25:50 +0200

    hi

--- src/algorithms/search/SearchableMaze.java
+++ src/algorithms/search/SearchableMaze.java
@@ -3,6 +3,7 @@ package algorithms.search;
 import java.util.ArrayList;
 
 public class SearchableMaze implements ISearchable{
+    private Maze M;
     @Override
     public ArrayList<State> getAllPossibleStates(State S) {
         return null;
