0fcd146fc959ef79f94ef1dddd03749062c8a5dd
navitbranin <navitbranin123@gmail.com>
2021-03-25 17:20:35 +0200

    1

---
+++ src/algorithms/mazeGenerators/AMazeGenerator.java
@@ -0,0 +1,9 @@
+package algorithms.mazeGenerators;
+
+public abstract class AMazeGenerator implements IMazeGenerator {
+
+    @Override
+    public long measureAlgorithmTimeMillis(int rows, int columns) {
+        return 0;
+    }
+}
---
+++ src/algorithms/mazeGenerators/Maze.java
@@ -0,0 +1,15 @@
+package algorithms.mazeGenerators;
+
+public class Maze {
+
+    private Position startPosition;
+    private Position goalPosition;
+
+    public Position getStartPosition() {
+        return startPosition;
+    }
+
+    public Position getGoalPosition() {
+        return goalPosition;
+    }
+}
