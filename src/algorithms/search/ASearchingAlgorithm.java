b91553abb4a5bd0160bbd6182c7fc2fa3cca6ac7
danahoh <danahoh3@gmail.com>
2021-03-30 20:20:55 +0300

    Merge remote-tracking branch 'origin/master'

--- src/algorithms/search/ASearchingAlgorithm.java
+++ src/algorithms/search/ASearchingAlgorithm.java
@@ -1,5 +1,7 @@
 package algorithms.search;
 
+import java.util.ArrayList;
+
 public abstract class ASearchingAlgorithm implements ISearchingAlgorithm{
     @Override
     public Solution solve(ISearchable s) {
@@ -9,5 +11,8 @@ public abstract class ASearchingAlgorithm implements ISearchingAlgorithm{
     @Override
     public int getNumberOfVisitedNodes() {
         return 0;
+    }
+    protected ArrayList<AState> findNeighbors (AState state){
+        return null;
     }
 }
