4a5aeb25b6ce45775f097bb8e862a0e7f8761bba
navitbranin <navitbranin123@gmail.com>
2021-05-06 18:04:54 +0300

    checking saving solutions

--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -6,8 +6,10 @@ import algorithms.search.*;
 import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ServerStrategySolveSearchProblem implements IServerStrategy{
+    static private AtomicInteger counter = new AtomicInteger(0);
     @Override
     public void applyStrategy(InputStream inFromClient, OutputStream outToClient) {
         try{
@@ -21,46 +23,50 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy{
             int cols = maze.getMaze()[0].length;
             String folderName = rows + "_" + cols;
             int fileNameInt = Arrays.hashCode((maze.toByteArray()));
-            String fileName = Integer.toString(fileNameInt);
+            String fileNameToPath = Integer.toString(fileNameInt);
             boolean checkDir = new File(tempDirectoryPath,folderName).exists();
+            //check if size folder is exist
             if(checkDir)
             {
-                boolean checkFile = new File(tempDirectoryPath + "\\" + folderName, fileName).exists();
+                boolean checkFile = new File(tempDirectoryPath + "\\" + folderName + "\\PathsToSol", fileNameToPath).exists();
                 if (checkFile)
                 {
-                    FileInputStream fileIn = new FileInputStream(tempDirectoryPath + "\\" + folderName+ "\\" + fileName);
+                    //reading solution from existing file
+                    FileInputStream fileIn = new FileInputStream(tempDirectoryPath + "\\" + folderName+ "\\PathToSol\\" + fileNameToPath);
                     ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-                    solution = (Solution) objectIn.readObject();
+                    String pathToSol = (String)objectIn.readObject();
+                    FileInputStream fileInSol = new FileInputStream(pathToSol);
+                    ObjectInputStream objectInSol = new ObjectInputStream(fileInSol);
+                    solution = (Solution)objectInSol.readObject();
                     objectIn.close();
+                    objectInSol.close();
                 }
-                else{
-                    solution = createSolution( maze,tempDirectoryPath +"\\"+folderName, fileName);
-                    String newFolder = tempDirectoryPath+ '\\' + folderName + '\\' + fileName;
-                    FileOutputStream fileOut = new FileOutputStream(newFolder);
-                    ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-                    objectOut.writeObject(solution);
-                    objectOut.close();
+                else {
+                    solution = getSolutionPath(maze, tempDirectoryPath, folderName, fileNameToPath);
                 }
             }
             else
             {
+                //creating folder in name of size
                 boolean ifCreated = new File(tempDirectoryPath+"\\"+folderName).mkdir();
                 if (ifCreated)
                 {
-                    String newDir = tempDirectoryPath+"\\"+folderName;
-                    solution = createSolution( maze, newDir, fileName);
-                    FileOutputStream fileOut = new FileOutputStream(newDir + "\\" + fileName);
-                    ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-                    objectOut.writeObject(solution);
-                    objectOut.close();
+                    boolean ifCreatedSolutions = new File(tempDirectoryPath+"\\"+folderName +"\\"+ "Solutions").mkdir();
+                    boolean ifCreatedPaths = new File(tempDirectoryPath+"\\"+folderName +"\\"+ "PathsToSol").mkdir();
+                    if(ifCreatedSolutions && ifCreatedPaths)
+                    {
+                        solution = getSolutionPath(maze, tempDirectoryPath, folderName, fileNameToPath);
+                    }
+                    else
+                    {
+                        throw new IOException();
+                    }
                 }
                 else
                 {
                     throw new IOException();
                 }
-
             }
-
             toClient.writeObject(solution);
            // toClient.flush();
             fromClient.close();
@@ -72,8 +78,19 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy{
 
     }
 
+    private Solution getSolutionPath(Maze maze, String tempDirectoryPath, String folderName, String fileNameToPath) throws Exception {
+        Solution solution;
+        String solPathFile = tempDirectoryPath+"\\"+folderName +"\\"+ "PathsToSol"+ "\\" + fileNameToPath;
+        String solPath = tempDirectoryPath+"\\"+folderName +"\\"+ "Solutions" + "\\" + "Sol_" + counter;
+        FileOutputStream fileOut = new FileOutputStream(solPathFile);
+        ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+        objectOut.writeObject(solPath);
+        solution = createSolution( maze, tempDirectoryPath,folderName + "\\Solutions", "Sol_" + counter);
+        return solution;
+    }
 
-    private Solution createSolution(Maze maze, String tempDirectoryPath, String fileName) throws Exception {
+
+    private Solution createSolution(Maze maze, String tempDirectoryPath,String folderName ,String fileName) throws Exception {
         SearchableMaze searchableMaze = new SearchableMaze(maze);
         ISearchingAlgorithm searcher;
         String searchingAlgo = Configurations.getConf().getSearchingAlgo();
@@ -90,6 +107,13 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy{
         {
             searcher = new BreadthFirstSearch();
         }
-        return searcher.solve(searchableMaze);
+        Solution solution = searcher.solve(searchableMaze);
+        String newDir = tempDirectoryPath+ "\\" + folderName + "\\" + fileName;
+        FileOutputStream fileOut = new FileOutputStream(newDir);
+        ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+        objectOut.writeObject(solution);
+        objectOut.close();
+        return solution;
     }
+
 }
