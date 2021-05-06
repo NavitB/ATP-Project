8ed9ca500007e89f6397ae88ddf882032ae12683
navitbranin <navitbranin123@gmail.com>
2021-05-06 15:37:33 +0300

    checking saving solutions

--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -16,41 +16,51 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy{
             ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
             Maze maze = (Maze) fromClient.readObject();
             String tempDirectoryPath = System.getProperty("java.io.tmpdir");
+            System.out.println(tempDirectoryPath);
+            int rows = maze.getMaze().length;
+            int cols = maze.getMaze()[0].length;
+            String folderName = rows + "*" + cols;
             int fileNameInt = Arrays.hashCode((maze.toByteArray()));
             String fileName = Integer.toString(fileNameInt);
-            //System.out.println(tempDirectoryPath+"\\"+fileName);
-            boolean check = new File(tempDirectoryPath, fileName).exists();
-            if (check)
+            boolean checkDir = new File(tempDirectoryPath,folderName).exists();
+            if(checkDir)
             {
-                FileInputStream fileIn = new FileInputStream(tempDirectoryPath+"\\"+fileName);
-                ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-                solution = (Solution) objectIn.readObject();
-                objectIn.close();
-            }
-            else{
-                SearchableMaze searchableMaze = new SearchableMaze(maze);
-                ISearchingAlgorithm searcher;
-                String searchingAlgo = Configurations.getConf().getSearchingAlgo();
-
-                if(searchingAlgo.equals("DepthFirstSearch"))
+                boolean checkFile = new File(tempDirectoryPath + "\\" + folderName, fileName).exists();
+                if (checkFile)
                 {
-                    searcher = new DepthFirstSearch();
+                    FileInputStream fileIn = new FileInputStream(tempDirectoryPath + "\\" + folderName+ "\\" + fileName);
+                    ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+                    solution = (Solution) objectIn.readObject();
+                    objectIn.close();
                 }
-                else if(searchingAlgo.equals("BestFirstSearch"))
+                else{
+                    solution = createSolution( maze,tempDirectoryPath +"\\"+folderName, fileName);
+                    String newFolder = tempDirectoryPath+ '\\' + folderName + '\\' + fileName;
+                    FileOutputStream fileOut = new FileOutputStream(newFolder);
+                    ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+                    objectOut.writeObject(solution);
+                    objectOut.close();
+                }
+            }
+            else
+            {
+                File newDir = new File(tempDirectoryPath ,folderName);
+                boolean ifCreated = newDir.mkdir();
+                if (ifCreated)
                 {
-                   searcher = new BestFirstSearch();
+                    solution = createSolution( maze, newDir.getPath(), fileName);
+                    FileOutputStream fileOut = new FileOutputStream(newDir.getPath() + "\\" + fileName);
+                    ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+                    objectOut.writeObject(solution);
+                    objectOut.close();
                 }
                 else
                 {
-                    searcher = new BreadthFirstSearch();
+                    throw new IOException();
                 }
-                solution = searcher.solve(searchableMaze);
-                FileOutputStream fileOut = new FileOutputStream(tempDirectoryPath+"\\"+fileName);
-                ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-                objectOut.writeObject(solution);
-                objectOut.close();
 
             }
+
             toClient.writeObject(solution);
            // toClient.flush();
             fromClient.close();
@@ -60,5 +70,26 @@ public class ServerStrategySolveSearchProblem implements IServerStrategy{
             e.printStackTrace();
         }
 
+    }
+
+
+    private Solution createSolution(Maze maze, String tempDirectoryPath, String fileName) throws Exception {
+        SearchableMaze searchableMaze = new SearchableMaze(maze);
+        ISearchingAlgorithm searcher;
+        String searchingAlgo = Configurations.getConf().getSearchingAlgo();
+
+        if(searchingAlgo.equals("DepthFirstSearch"))
+        {
+            searcher = new DepthFirstSearch();
+        }
+        else if(searchingAlgo.equals("BestFirstSearch"))
+        {
+            searcher = new BestFirstSearch();
+        }
+        else
+        {
+            searcher = new BreadthFirstSearch();
+        }
+        return searcher.solve(searchableMaze);
     }
 }
